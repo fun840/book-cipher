125bdc8d90654fd71d8bd80f3eb4b44a062a9395
fun840 <fun840@protonmail.com>
2024-07-23 13:28:50 -0400

    Format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,17 +16,17 @@
 	}
 </script>
 
-<div class="p-3 flex h-screen w-screen flex-col gap-3 items-center">
+<div class="flex h-screen w-screen flex-col items-center gap-3 p-3">
 	<p class="m-5 mt-4 text-lg text-white">Book cipher</p>
 
-	<div class="flex gap-3 w-full flex-grow">
+	<div class="flex w-full flex-grow gap-3">
 		<Textarea id="book" placeholder="Book text..." bind:value={bookText} />
 		<Textarea id="cipher" placeholder="Cipher text..." bind:value={cipherText} />
 	</div>
 
 	<Button class="px-10" on:click={decode}>Decode</Button>
 
-	<div class="flex justify-center w-full">
+	<div class="flex w-full justify-center">
 		<Textarea id="output" rows="12" placeholder="Output" bind:value={output}></Textarea>
 	</div>
 </div>
